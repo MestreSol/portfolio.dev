e435338422f1235fbbc99e153eb591b2e0af883d
MestreSol <mb.muitobosta@gmail.com>
2025-01-12 00:40:38 -0300

    Configure lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next",
+        "plugin:react-hooks/recommended",
+        "next/typescript",
+        "plugin:@next/next/recommended",
+        "plugin:eslint-plugin-react/recommended"
+
+
+    ),
 ];
 
 export default eslintConfig;
