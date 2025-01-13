764356fb24c7e252fe316a36466786f23a3c93de
MestreSol <mb.muitobosta@gmail.com>
2025-01-13 20:01:06 -0300

    Update Global Style

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Profile from "@/components/Profile";
 
 export default function Home() {
-    return (
-        <div className="container">
-            <div className="profile-area">
-                <Image
-                    src="/profile.jpg"
-                    alt="Picture of the author"
-                    width={200}
-                    height={200}
-                    className="profile-image"
-                />
-                <h1 className="name">Your Name</h1>
-                <p className="bio">Your Bio</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
 }
