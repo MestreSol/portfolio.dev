9daef65423a92a728cb3c8e9f6a1427f72b8a4f9
MestreSol <mb.muitobosta@gmail.com>
2025-01-13 20:01:21 -0300

    Add Portfolio component and associated styles

---
+++ app/portifolio/page.tsx
@@ -0,0 +1,12 @@
+import Profile from "@/components/Profile";
+import style from "./page.module.css";
+
+export default function Portifolio() {
+  return (
+    <div>
+      <div className={style.container}>
+        <Profile />
+      </div>
+    </div>
+  );
+}
