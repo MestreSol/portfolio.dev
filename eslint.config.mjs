934ab255fbe423caa48a4283efeefc20b078c87b
MestreSol <mb.muitobosta@gmail.com>
2025-01-13 19:58:35 -0300

    Update Configure Project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends(
-        "next/core-web-vitals",
-        "next",
-        "plugin:react-hooks/recommended",
-        "next/typescript",
-        "plugin:@next/next/recommended",
-        "plugin:eslint-plugin-react/recommended",
-        "plugin:eslint-prettier/recommended",
-    ),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next",
+    "plugin:react-hooks/recommended",
+    "next/typescript",
+    "plugin:@next/next/recommended",
+    "plugin:eslint-plugin-react/recommended",
+    "plugin:eslint-prettier/recommended",
+  ),
 ];
 
 export default eslintConfig;
