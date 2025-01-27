bf620160eac86fa2014fb57bf92e7da9d30441d2
MestreSol <mb.muitobosta@gmail.com>
2025-01-27 14:14:06 -0300

    Add configuration files, update styles, and implement metadata generation for improved project structure and SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,38 @@
 import React from "react";
 import "./globals.css";
+import { baseURL, home, person } from "@/app/resources";
 import GlobalMenu from "@/components/area/globalMenu";
+
+export async function generateMetadata() {
+  return {
+    metadataBase: new URL(`https://${baseURL}`),
+    title: {
+      default: home.title,
+      template: "%s | Portfy",
+    },
+    description: home.description,
+    openGraph: {
+      title: `${person.firstName}'s Portfolio`,
+      description: "Portfolio website showcasing my work.",
+      url: baseURL,
+      siteName: `${person.firstName}'s Portfolio`,
+      locale: "en_US",
+      type: "website",
+      image: ["/api/og?title=portfy"],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
 export default function RootLayout({
   children,
 }: Readonly<{
