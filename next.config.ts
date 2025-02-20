5f041f95da90f45db26116d9db8b48efc6970b62
MestreSol <mb.muitobosta@gmail.com>
2025-02-20 19:58:21 -0300

    Add GitHub avatars domain to image configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'i.redd.it', 'cdn.mos.cms.futurecdn.net', 'picsum.photos', 'i.pinimg.com'],
+    domains: ['images.unsplash.com', 'i.redd.it', 'cdn.mos.cms.futurecdn.net', 'picsum.photos', 'i.pinimg.com', 'avatars.githubusercontent.com'],
   }
 }
 export default nextConfig
