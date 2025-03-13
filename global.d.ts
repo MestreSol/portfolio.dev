895bccd475c09be03d556dd658e0bb56ffcf2d39
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2025-03-13 17:34:04 +0000

    Adicionar testes para a API de portfólios e atualizar dependências no package.json

--- global.d.ts
+++ global.d.ts
@@ -155,6 +155,166 @@ declare global {
     skills: Skill[]
     courses: Course[]
   }
+
+  type Portfolio = {
+    id: number
+    user: {
+      id: number
+      name: string
+      email: string
+      password: string
+    }
+    avatar: string
+    cover: string
+    description: string
+    about: string
+    socials: {
+      id: number
+      user: {
+        id: number
+        name: string
+        email: string
+        password: string
+      }
+      name: string
+      link: string
+    }[]
+    location: string
+    contacts: string[]
+    nodes: number
+    workExperience: {
+      id: number
+      user: User
+      company: Company
+      location: string
+      startDate: string
+      endDate: string
+      activities: {
+        id: number
+        description: string
+      }[]
+      coWorkers: {
+        id: number
+        name: string
+        role: string
+        avatar: string
+      }[]
+      usedSkills: {
+        id: number
+        name: string
+        proficiency: string
+      }[]
+    }[]
+    academicExperience: {
+      id: number
+      course: string
+      user: {
+        id: number
+        name: string
+        email: string
+        password: string
+      }
+      level: string
+      degree: string
+      institution: string
+      location: string
+      startDate: string
+      endDate: string
+      activities: {
+        id: number
+        description: string
+      }[]
+      coWorkers: {
+        id: number
+        name: string
+        role: string
+        avatar: string
+      }[]
+      image: string // Adicionando a propriedade 'image'
+    }[]
+    followers: {
+      id: number
+      name: string
+      email: string
+      password: string
+    }[]
+    projects: {
+      id: number
+      about: string
+      Team: {
+        id: number
+        name: string
+        description: string
+        image: string
+        members: {
+          id: number
+          user: {
+            id: number
+            name: string
+            email: string
+            password: string
+          }
+          role: string
+        }[]
+      }[]
+      user: {
+        id: number
+        name: string
+        email: string
+        password: string
+      }
+      name: string
+      description: string
+      startDate: string
+      endDate: string
+      activities: {
+        id: number
+        description: string
+      }[]
+      coWorkers: {
+        id: number
+        name: string
+        role: string
+        avatar: string
+      }[]
+      image: string
+    }[]
+    languages: {
+      id: number
+      name: string
+      description: string
+      proficiency: number
+    }[]
+    skills: {
+      id: number
+      name: string
+      description: string
+      proficiency: number
+      verified: string
+      projects: number
+      works: number
+    }[]
+    courses: {
+      id: number
+      name: string
+      description: string
+      emissor: string
+      level: string
+      startDate: string
+      endDate: string
+      activities: {
+        id: number
+        description: string
+      }[]
+      coWorkers: {
+        id: number
+        name: string
+        role: string
+        avatar: string
+      }[]
+      certify: string
+    }[]
+  }
 }
 
 export {}
